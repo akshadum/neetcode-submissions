fd3d39b07752e754b0ff863c019e2e1d9c7d84b2
akshadum <75430122+akshadum@users.noreply.github.com>
2026-07-25 01:46:23 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        if( nums[0] == 0 && nums.length == 1)
+            return 1;
+        TreeSet<Integer> set = new TreeSet<>();
+		int count = 1, maxCount = 1;
+		for(int i:nums) set.add(i);
+		List<Integer> list = new ArrayList<>(set);
+
+		for(int i=1; i<list.size(); i++) {
+			if(list.get(i) == list.get(i-1) + 1){
+				count++;
+			}
+			else count = 1;
+
+			maxCount = Math.max(maxCount, count);
+		}
+        return maxCount;
+        
+    }
+}
