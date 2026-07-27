8b64dea8dc6a130add8193ef12caf0cc52f50e58
Karan Umredkar <75430122+akshadum@users.noreply.github.com>
2026-07-28 02:14:18 +0530

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i: nums)
+            list.add(i);
+        return (int)list.indexOf(target);
+    }
+}
